9b186c237e9cbcb8d0a572eeceff0af8f2e69baa
Isadora Silva <silvairs9292@gmail.com>
2024-05-19 22:15:28 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const generatePasswordButton
+const generatePasswordElement
+
+const openCloseGeneratorButton
+
+const
+const
+const
+const
+const
+const
+
+
+const
+return
+
+const
+return
+
+
+const
+return
+
+const
+return
+
+
+const
+const
+if
+if
+if
+if
+
+for
+
